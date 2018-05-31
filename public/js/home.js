0419f97d9c17c0ae33c45a646130ae396be5ef96
Nangxi <575981390@qq.com>
2018-05-31 11:53:43 +0800

    解决主页面刷新时loading可以滚动的问题

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,5 @@
 //在页面未加载完毕之前显示的loading Html自定义内容
-var _LoadingHtml = '<div id="loadingDiv" style=" position:absolute; top: 0px; left: 0px;width:100%;height:100%;background:white;z-index:10000;"><div style="margin:auto; position: absolute; left:0;top:0; right:0;bottom:0;cursor1: wait;  width: 100px; height: 100px; background: url(/public/img/loading.gif) no-repeat;background-size: cover;"></div></div>';
+var _LoadingHtml = '<div id="loadingDiv" style=" position:fixed; top: 0px; left: 0px;width:100%;height:100%;background:white;z-index:10000;"><div style="margin:auto; position: absolute; left:0;top:0; right:0;bottom:0;cursor1: wait;  width: 100px; height: 100px; background: url(/public/img/loading.gif) no-repeat;background-size: cover;"></div></div>';
 //呈现loading效果
 document.write(_LoadingHtml);
 
